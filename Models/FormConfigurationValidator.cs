5cab814b90220dead1565ad6656b2aa858b2705b
Valentina Codutti Theran <valen-codutti@hotmail.com>
2024-11-27 22:57:23 -0500

    Create Validator class

---
+++ Models/FormConfigurationValidator.cs
@@ -0,0 +1,69 @@
+﻿using DynamicFormBlazor.Models;
+
+namespace DynamicFormBlazor.Services
+{
+    public class FormConfigurationValidator
+    {
+        public void Validate(FormConfiguration formConfiguration)
+        {
+            if (formConfiguration == null)
+            {
+                throw new Exception("The JSON input cannot be null or empty.");
+            }
+
+            if (string.IsNullOrEmpty(formConfiguration.Title))
+            {
+                throw new Exception("The property Title of the JSON cannot be null or empty.");
+            }
+
+            if (formConfiguration.Fields == null)
+            {
+                throw new Exception("The property Fields of the JSON cannot be null.");
+            }
+
+            if (formConfiguration.Fields.Count == 0)
+            {
+                throw new Exception("The property Fields of the JSON should have at least one field.");
+            }
+
+            foreach (var field in formConfiguration.Fields)
+            {
+                ValidateField(field);
+            }
+        }
+
+        private void ValidateField(FormField formField)
+        {
+            if (string.IsNullOrEmpty(formField.Label))
+            {
+                throw new Exception($"No Label specification for {formField.Type}. Please provide one to continue.");
+            }
+
+            if (formField.Type.Equals("number", StringComparison.OrdinalIgnoreCase))
+            {
+                if (formField.Min < 0)
+                {
+                    throw new Exception("The value for property Min cannot be less than 0.");
+                }
+
+                if (formField.Max < 0)
+                {
+                    throw new Exception("The value for property Max cannot be less than 0.");
+                }
+
+                if (formField.Min > formField.Max)
+                {
+                    throw new Exception("The value for Min cannot be greater than the value for Max.");
+                }
+            }
+
+            if (formField.Type.Equals("dropdown", StringComparison.OrdinalIgnoreCase))
+            {
+                if (formField.Values == null || !formField.Values.Any())
+                {
+                    throw new Exception("The dropdown field should have at least one value.");
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<DataTransferService>();
+builder.Services.AddScoped<FormConfigurationValidator>();
 
 await builder.Build().RunAsync();
